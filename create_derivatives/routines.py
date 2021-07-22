f5f930d805ff6d2db2e617bb7a60a135a34cce5c
bonniegee <bgordon@rockarch.org>
2021-07-22 17:01:55 -0400

    break out services

--- create_derivatives/routines.py
+++ create_derivatives/routines.py
@@ -1,28 +1,30 @@
-class IIIFPipeline:
-    def __init__(self):
-        # TO DO: instantiate AS client
-        # TO DO: instantiate AWS client
-        # TO DO: verify and set directories
-    
-    def run(self):
-       # TO DO: list to iterate through
-       # TO DO: restructure bag
-       # TO DO: get data from ArchivesSpace
-       # TO DO: get UUID
-       # TO DO: make derivatives
-       # TO DO: make manifests
-       # TO DO: make PDFs
-       # TO DO: upload files and PDFs
-       
 
-
-    
-    def restructure_bag(self):
-        """docstring for restructure_bag"""
+class BagPreparer:
+    # TO DO: restructure bag
+    # TO DO: get data from ArchivesSpace
+    # TO DO: get UUID
     pass
-   
-   
+
+
+class JP2Maker:
+    # TO DO: make JPG2000 derivatives
+    pass
+
+
+class PDFMaker:
+    # TO DO: make PDF derivates, compress, OCR
+    pass
+
+
+class ManifestMaker:
+    # TO DO: make manifests
+    pass
+
+
+class AWSUpload:
+    # TO DO: upload files and PDFs
+    pass
+
+
 class CleanupRoutine:
     pass
-        
-        
