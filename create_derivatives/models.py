d6f1e5524c9e207a0311405b6b94ccf654e3f034
bonniegee <bgordon@rockarch.org>
2021-07-23 10:26:56 -0400

    more setup

--- create_derivatives/models.py
+++ create_derivatives/models.py
@@ -3,5 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Bag(models.Model):
+    bag_path = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now=True)
+    last_modified = models.DateTimeField(auto_now_add=True)
+
+
 class User(AbstractUser):
     pass
