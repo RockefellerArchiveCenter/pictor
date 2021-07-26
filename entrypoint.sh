f97de4a8edbf2a56e51f272ae64e2f6bd09b8083
Hillel Arnold <helrond@hotmail.com>
2021-07-26 14:52:58 -0400

    check for config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Apply database migrations
+./wait-for-it.sh db:5432 -- echo "Checking for config file"
+
+# Create config file if it doesn't exist
+if [ ! -f pictor/config.py ]; then
+    cp pictor/config.py.example pictor/config.py
+fi
+
 echo "Apply database migrations"
-./wait-for-it.sh db:5432 -- python manage.py migrate
+python manage.py migrate
 
 #Start server
 echo "Starting server"
