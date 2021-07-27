2b9f91f91b29b0fc691362104db6d6c13991f7cd
Hillel Arnold <helrond@hotmail.com>
2021-07-27 09:46:56 -0400

    deal with linting

--- create_derivatives/admin.py
+++ create_derivatives/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bag
+
+
+@admin.register(Bag)
+class BagAdmin(admin.ModelAdmin):
+    """Django Admin for Bags."""
+    pass
