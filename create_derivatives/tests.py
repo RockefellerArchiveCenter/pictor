aeaf742d6453e42eb18b07baa49b30605489be28
Hillel Arnold <helrond@hotmail.com>
2021-07-26 20:58:19 -0400

    finish tests

--- create_derivatives/tests.py
+++ create_derivatives/tests.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+from unittest.mock import patch
 
 from django.test import TestCase
 from pictor import settings
@@ -17,16 +18,12 @@ class HelpersTestCase(TestCase):
         for p in test_paths:
             path = Path(p)
             path.mkdir(exist_ok=True)
-            self.assertTrue(
-                check_dir_exists(p),
-                "Function did not return True")
+            self.assertTrue(check_dir_exists(p), "Function did not return True")
             path.rmdir()
             with self.assertRaises(Exception) as context:
                 check_dir_exists(p)
             self.assertTrue(
-                p in str(
-                    context.exception),
-                "Directory was not found in exception")
+                p in str(context.exception), "Directory was not found in exception")
 
 
 class BagPreparerTestCase(TestCase):
@@ -40,35 +37,41 @@ class BagPreparerTestCase(TestCase):
         for f in Path("create_derivatives", "fixtures", "bags").iterdir():
             shutil.copy(str(f), settings.SRC_DIR)
 
-    def test_unpack_bag(self):
+    @patch("create_derivatives.clients.ArchivesSpaceClient.__init__")
+    def test_unpack_bag(self, mock_init):
         """Asserts bags are correctly unpacked."""
+        mock_init.return_value = None
         routine = BagPreparer()
         for bag in Bag.objects.filter(process_status=Bag.CREATED):
             created = routine.unpack_bag(bag.bag_identifier)
             self.assertTrue(
-                Path(
-                    settings.TMP_DIR,
-                    bag.bag_identifier).exists())
+                Path(settings.TMP_DIR, bag.bag_identifier).exists(),
+                "Extracted path does not exist")
             self.assertEqual(
-                created, str(
-                    Path(settings.TMP_DIR, bag.bag_identifier)))
+                created, str(Path(settings.TMP_DIR, bag.bag_identifier)),
+                "Wrong bag path returned")
 
-    def test_run(self):
-        """Asserts that run method produces the desired results.
+    @patch("create_derivatives.clients.ArchivesSpaceClient.__init__")
+    @patch("create_derivatives.clients.ArchivesSpaceClient.get_object")
+    def test_run(self, mock_get_object, mock_init):
+        """Asserts that the run method produces the desired results.
 
         Tests that the correct number of bags was processsed, and that the
         attributes of each have been correctly set.
         """
+        mock_init.return_value = None
+        as_data = {"uri": "foobar"}
+        mock_get_object.return_value = as_data
         created_len = len(Bag.objects.filter(process_status=Bag.CREATED))
         prepared = BagPreparer().run()
         self.assertTrue(isinstance(prepared, tuple))
         self.assertEqual(prepared[0], "Bags successfully prepared")
-        self.assertEqual(len(prepared[1]), created_len)
-        self.assertTrue(len(Path(settings.TMP_DIR).iterdir()), created_len)
+        self.assertEqual(len(prepared[1]), created_len, "Wrong number of bags processed")
+        self.assertTrue(len(list(Path(settings.TMP_DIR).glob("*"))), created_len)
         for bag in Bag.objects.all():
             self.assertEqual(bag.process_status, Bag.PREPARED)
-            self.assertIsNot(bag.bag_path, None)
-            self.assertIsNot(bag.as_data, None)
+            self.assertEqual(bag.bag_path, str(Path(settings.TMP_DIR, bag.bag_identifier)))
+            self.assertEqual(bag.as_data, as_data)
             self.assertIsNot(bag.dimes_identifier, None)
 
     def tearDown(self):
