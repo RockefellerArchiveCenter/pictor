fb746f84d2c1c8a20f3f044b17465a6a48026fa1
Hannah <hsistrunk@gmail.com>
2021-08-04 11:12:07 -0400

    add JP2Maker routine

--- create_derivatives/helpers.py
+++ create_derivatives/helpers.py
@@ -5,3 +5,25 @@ def check_dir_exists(dir):
     if not Path(dir).is_dir():
         raise Exception("Expected directory {} does not exist".format(dir))
     return True
+
+
+def matching_files(directory, prefix=None, suffix=None,
+                   prepend=False):
+    """Get a list of files that start with a specific prefix.
+    Args:
+        directory (str): The directory containing files.
+        prefix (str): A prefix to match filenames against.
+        suffix (str): A suffix (file extension) to match filenames against.
+        prepend (bool): Add the directory to the filepaths returned
+    Returns:
+        files (lst): a list of files that matched the identifier.
+    """
+    directory_path = Path(directory)
+    files = sorted([f for f in directory_path.iterdir() if (
+        Path(directory, f).is_file() and not str(f).startswith((".", "Thumbs")))])
+    if prefix:
+        files = sorted([f for f in files if str(f).startswith(prefix)])
+    if suffix:
+        files = sorted([f for f in files if str(f).endswith(suffix)])
+    return [Path(directory, f) for f in files] if prepend else files
+
--- create_derivatives/routines.py
+++ create_derivatives/routines.py
@@ -4,16 +4,17 @@ import bagit
 import shortuuid
 from asterism.file_helpers import anon_extract_all
 from pictor import settings
+from PIL import Image
 
 from .clients import ArchivesSpaceClient
-from .helpers import check_dir_exists
+from .helpers import check_dir_exists, matching files
 from .models import Bag
 
 
 class BagPreparer:
     """Prepares bags for derivative creation.
 
-    Unpacks bags into settings.TMP_DIR and adds all necessary data the object.
+    Unpacks bags into settings.TMP_DIR and adds all necessary data to the object.
 
     Returns:
         A tuple containing human-readable message along with list of bag identifiers.
@@ -63,8 +64,74 @@ class BagPreparer:
 
 
 class JP2Maker:
-    # TO DO: make JPG2000 derivatives
-    pass
+    """Creates JP2000 derivatives from TIFFs.
+
+    Creates JP2 directory in bag's data directory and JP2 derivatives.
+
+    Returns:
+        JP2000 derivatives in the bags' /data directory.
+        Updated bag process status.
+    """
+
+    def run(self):
+    bags_with_jp2s = []
+    for bag in Bag.objects.filter(process_status=Bag.PREPARED):
+        if Path.is_dir(bag.bag_path, "data", "service"):
+            tiff_files_dir = str(Path(bag.bag_path, "data", "service"))
+        else:
+            tiff_files_dir = str(Path(bag.bag_path, "data"))
+        self.jp2_path = self.create_jp2(bag, tiff_files)
+        bag.process_status = Bag.JPG2000
+        bag.save()
+        bags_with_jp2s.append(bag.bag_identifier)
+    msg = "JPG2000s created." if len(bags_with_jp2s) else "No TIFF files ready for JP2 creation."
+    return msg, bags_with_jp2s
+
+
+    def calculate_layers(self):
+    """Calculates the number of layers based on pixel dimensions.
+    For TIFF files, image tag 256 is the width, and 257 is the height.
+    Args:
+        file (str): filename of a TIFF image file.
+    Returns:
+        layers (int): number of layers to convert to
+    """
+
+    with Image.open(file) as img:
+        width = [w for w in img.tag[256]][0]
+        height = [h for h in img.tag[257]][0]
+    return math.ceil((math.log(max(width, height)) / math.log(2)
+                      ) - ((math.log(96) / math.log(2)))) + 1
+
+
+    def create_jp2(self, bag, tiff_files_dir):
+    """Creates JPEG2000 files from TIFF files.
+    The default options for conversion below are:
+    - Compression ration of `1.5`
+    - Precinct size: `[256,256]` for first two layers and then `[128,128]` for all others
+    - Code block size of `[64,64]`
+    - Progression order of `RPCL`
+    """
+
+    default_options = ["-r", "1.5",
+                    "-c", "[256,256],[256,256],[128,128]",
+                    "-b", "64,64",
+                    "-p", "RPCL"]
+    tiff_files = matching_files(tiff_files_dir, prepend=True)
+    jp2_dir = Path(bag.bag_path, "data", "JP2")
+    if not jp2_dir.is_dir():
+        jp2_dir.mkdir()
+    # I feel like this is wrong. It doesn't make sense to use a for loop here, does it?
+    for file in tiff_files:
+        jp2_path = "{}.jp2".format(Path(jp2_dir, bag.dimes_identifier))
+        layers = calculate_layers(file)
+        cmd = ["/usr/local/bin/opj_compress",
+            "-i", file,
+            "-o", jp2_path,
+            "-n", str(layers),
+            "-SOP"] + default_options
+        subprocess.run(cmd, check=True)
+        return jp2_path
 
 
 class PDFMaker:
