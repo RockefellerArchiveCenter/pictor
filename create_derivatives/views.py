8fe0f24b3d2a0108da0d250b4788e94287c3252b
Hillel Arnold <helrond@hotmail.com>
2021-08-10 09:00:22 -0400

    add routine views and URL config

--- create_derivatives/views.py
+++ create_derivatives/views.py
@@ -1,6 +1,9 @@
+from asterism.views import RoutineView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Bag
+from .routines import (AWSUpload, BagPreparer, Cleanup, JP2Maker,
+                       ManifestMaker, PDFMaker)
 from .serializers import BagDetailSerializer, BagListSerializer
 
 
@@ -20,3 +23,33 @@ class BagViewSet(ModelViewSet):
         request.data["data"] = request.data.get("bag_data")
         request.data["process_status"] = Bag.CREATED
         return super().create(request, *args, **kwargs)
+
+
+class BagPreparerView(RoutineView):
+    """Runs the BagPreparer routine. Accepts POST requests only."""
+    routine = BagPreparer
+
+
+class JP2MakerView(RoutineView):
+    """Runs the JP2Maker routine. Accepts POST requests only."""
+    routine = JP2Maker
+
+
+class PDFMakerView(RoutineView):
+    """Runs the PDFMaker routine. Accepts POST requests only."""
+    routine = PDFMaker
+
+
+class ManifestMakerView(RoutineView):
+    """Runs the ManifestMaker routine. Accepts POST requests only."""
+    routine = ManifestMaker
+
+
+class AWSUploadView(RoutineView):
+    """Runs the AWSUpload routine. Accepts POST requests only."""
+    routine = AWSUpload
+
+
+class CleanupView(RoutineView):
+    """Runs the Cleanup routine. Accepts POST requests only."""
+    routine = Cleanup
